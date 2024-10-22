1cc166ee4d99fccfecaf21b9ddf43d54465bd555
Pedro Garcia <37590939+PedroGarciaOT@users.noreply.github.com>
2024-10-22 11:00:39 -0400

    fixing linux path

--- fcli-sample-pipeline.sh
+++ fcli-sample-pipeline.sh
@@ -29,9 +29,9 @@ tar -pxzf fcli-linux.tgz
 source ./fcli_completion
 
 # Install tools
-fcli tool sc-client install --version latest
+./fcli tool sc-client install --version latest
 
-fcli tool fod-uploader install --version latest
+./fcli tool fod-uploader install --version latest
 
 echo "BUILD TASKS"
 cd ./TargetApplication
@@ -46,32 +46,32 @@ cd ../
 
 echo "POST-BUILD TASKS"
 # Login into FoD
-#fcli fod session login --url ${FOD_URL} --client-id ${FOD_CLIENT_ID} --client-secret ${FOD_CLIENT_SECRET} -k
-fcli fod session login --url ${FOD_URL} --user ${FOD_USER} --password ${FOD_PASSWORD} --tenant ${FOD_TENANT} -k
+#./fcli fod session login --url ${FOD_URL} --client-id ${FOD_CLIENT_ID} --client-secret ${FOD_CLIENT_SECRET} -k
+./fcli fod session login --url ${FOD_URL} --user ${FOD_USER} --password ${FOD_PASSWORD} --tenant ${FOD_TENANT} -k
 
 # Create an application
-# fcli fod app create ${FOD_APPLICATION} --auto-required-attrs --criticality "High" -d "FCLI created application" --delim "application:release" --owner ${FOD_USER} --release ${FOD_RELEASE} --release-description "FCLI created release" --status "Production" --type "Web" --store fodapp 
+# ./fcli fod app create ${FOD_APPLICATION} --auto-required-attrs --criticality "High" -d "FCLI created application" --delim "application:release" --owner ${FOD_USER} --release ${FOD_RELEASE} --release-description "FCLI created release" --status "Production" --type "Web" --store fodapp 
 
 # Print application details
-# fcli util variable contents fodapp -o json
+# ./fcli util variable contents fodapp -o json
 
 # Create release
-fcli fod release create ${FOD_APPLICATION%:%FOD_NEW_RELEASE} --status Production  --skip-if-exists --store fodrel
+./fcli fod release create ${FOD_APPLICATION%:%FOD_NEW_RELEASE} --status Production  --skip-if-exists --store fodrel
 
 # Print release details
-fcli util variable contents fodrel -o json
+./fcli util variable contents fodrel -o json
 
 # Setup subscription
-fcli fod sast setup --release "::fodrel::" --assessment-type "Static Assessment" --frequency "Subscription" --entitlement-id ${FOD_ENTITLEMENT_ID} --technology-stack "Auto Detect" --audit-preference "Automated" --use-aviator -o json
+./fcli fod sast setup --release "::fodrel::" --assessment-type "Static Assessment" --frequency "Subscription" --entitlement-id ${FOD_ENTITLEMENT_ID} --technology-stack "Auto Detect" --audit-preference "Automated" --use-aviator -o json
 
 # Start scan
-fcli fod sast-scan start --release "::fodrel::" -f package.zip --store fodscan
+./fcli fod sast-scan start --release "::fodrel::" -f package.zip --store fodscan
 
 # Wait for scan to finish
-fcli fod sast-scan wait-for "::fodscan::scanId"
+./fcli fod sast-scan wait-for "::fodscan::scanId"
 
 # Run quality gate
-fcli fod action run check-policy --release "::fodrel::"
+./fcli fod action run check-policy --release "::fodrel::"
 
 # Logout from FoD
-fcli fod session logout
+./fcli fod session logout

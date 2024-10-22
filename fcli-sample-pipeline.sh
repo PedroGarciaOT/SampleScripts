d23e030ff92f12ba440fd0524bc23876770d1a0f
Pedro Garcia <37590939+PedroGarciaOT@users.noreply.github.com>
2024-10-22 13:08:23 -0400

    adding stdout

--- fcli-sample-pipeline.sh
+++ fcli-sample-pipeline.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 clear
+echo "-========== PIPELINE SETTINGS ==========-"
 # export environment variables 
 export PATH=${HOME}/fortify/tools/bin:${PATH}
 export FOD_URL="https://api.ams.fortify.com/"
 # export FOD_CLIENT_ID=
 # export FOD_CLIENT_SECRET=
-export FOD_USER=
-export FOD_PASSWORD=
-export FOD_TENANT=
+# export FOD_USER=
+# export FOD_PASSWORD=
+# export FOD_TENANT=
 
 # Entitlement Id is available at https://ams.fortify.com/Admin/Entitlements
-export FOD_ENTITLEMENT_ID=
+# export FOD_ENTITLEMENT_ID=
 
-export FOD_APPLICATION=
-export FOD_RELEASE=
-export FOD_NEW_RELEASE=
+# export FOD_NEW_APPLICATION=
+# export FOD_NEW_APPLICATION_RELEASE=
+
+# export FOD_APPLICATION=
+# export FOD_RELEASE=
 
 # use setenv script to set environment variables
 export ENV_FILE="./setenv.sh"
 if [ -f ${ENV_FILE} ]; then 
+    echo "ENV_FILE=${ENV_FILE}"
     sudo chmod +x ${ENV_FILE}
-    ${ENV_FILE}
+    ${ENV_FILE}    
 fi
 
-echo "PRE-BUILD TASKS"
+echo " PATH=${PATH}"
+echo " FOD_URL=${FOD_URL}"
+echo " FOD_CLIENT_ID=${FOD_CLIENT_ID}"
+echo " FOD_USER=${FOD_USER}"
+echo " FOD_TENANT=${FOD_TENANT}"
+echo " FOD_ENTITLEMENT_ID=${FOD_ENTITLEMENT_ID}"
+echo " FOD_NEW_APPLICATION=${FOD_NEW_APPLICATION}"
+echo " FOD_NEW_APPLICATION_RELEASE=${FOD_NEW_APPLICATION_RELEASE}"
+echo " FOD_APPLICATION=${FOD_APPLICATION}"
+echo " FOD_RELEASE=${FOD_RELEASE}"
+
+echo "-========== PRE-BUILD TASKS ==========-"
 # Clone Repo
 git clone -b main https://github.com/fortify/IWA-Java.git ./TargetApplication
 
@@ -37,7 +52,7 @@ source ./fcli_completion
 
 ./fcli tool fod-uploader install --version latest
 
-echo "BUILD TASKS"
+echo "-========== BUILD TASKS ==========-"
 cd ./TargetApplication
 
 # Build and resolve dependencies
@@ -48,19 +63,19 @@ scancentral package -o ../package.zip
 
 cd ../
 
-echo "POST-BUILD TASKS"
+echo "-========== POST-BUILD TASKS ==========-"
 # Login into FoD
 #./fcli fod session login --url ${FOD_URL} --client-id ${FOD_CLIENT_ID} --client-secret ${FOD_CLIENT_SECRET} -k
 ./fcli fod session login --url ${FOD_URL} --user ${FOD_USER} --password ${FOD_PASSWORD} --tenant ${FOD_TENANT} -k
 
 # Create an application
-# ./fcli fod app create ${FOD_APPLICATION} --auto-required-attrs --criticality "High" -d "FCLI created application" --delim "application:release" --owner ${FOD_USER} --release ${FOD_RELEASE} --release-description "FCLI created release" --status "Production" --type "Web" --store fodapp 
+# ./fcli fod app create ${FOD_NEW_APPLICATION} --auto-required-attrs --criticality "High" -d "FCLI created application" --delim "application:release" --owner ${FOD_USER} --release ${FOD_NEW_APPLICATION_RELEASE} --release-description "FCLI created release" --status "Production" --type "Web" --store fodapp 
 
 # Print application details
 # ./fcli util variable contents fodapp -o json
 
 # Create release
-./fcli fod release create ${FOD_APPLICATION%:%FOD_NEW_RELEASE} --status Production  --skip-if-exists --store fodrel
+./fcli fod release create ${FOD_APPLICATION%:%FOD_RELEASE} --status Production  --skip-if-exists --store fodrel
 
 # Print release details
 ./fcli util variable contents fodrel -o json

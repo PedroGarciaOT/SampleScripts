090129f47abe62459fcdb2f3da2386584e00ee55
Pedro Garcia <37590939+PedroGarciaOT@users.noreply.github.com>
2024-11-29 18:50:12 -0500

    removing unnecessary lines

--- Fortify on Demand/fcli-sample-oss-import.bat
+++ Fortify on Demand/fcli-sample-oss-import.bat
@@ -13,7 +13,4 @@ fcli fod session login --url %FOD_URL% --user %FOD_USER% --password %FOD_PASSWOR
 
 fcli fod oss-scan import-debricked --branch %DEBRICKED_OSS_BRANCH% --repository %DEBRICKED_OSS_REPO% --release %FOD_DEBRICKED_IMPORT_TARGET_RELEASE_ID% --debricked-access-token %DEBRICKED_PAT% --file "debricked-sbom.json" --chunk-size 20000000 --connect-timeout "3m" --socket-timeout "3m" --progress "auto" -k
 
-REM --store importdata
-REM fcli util variable contents importdata -o json
-
 fcli fod session logout

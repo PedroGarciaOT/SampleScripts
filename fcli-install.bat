a32424faa27043fc3ee31461ed83c833fd1e0068
Pedro Garcia <37590939+PedroGarciaOT@users.noreply.github.com>
2025-06-02 11:20:06 -0400

    Adding fcli-install script.

---
+++ fcli-install.bat
@@ -0,0 +1,40 @@
+ECHO OFF
+cls
+ECHO "-========== INSTALL FCLI AND TOOLS SETTINGS ==========-"
+REM # Set environment variables 
+SET PATH=%USERPROFILE%\fortify\tools\bin;%PATH%
+SET SC_CLIENT_VERSION=24.4.1
+SET DEBRICKED_CLI_VERSION=2.6.7
+
+ECHO "======================================================================"
+ECHO " PATH=%PATH%"
+ECHO "SC_CLIENT_VERSION=%SC_CLIENT_VERSION%"
+ECHO "DEBRICKED_CLI_VERSION=%DEBRICKED_CLI_VERSION%"
+ECHO "======================================================================"
+
+REM # Donwload and unpack fcli
+curl -sL https://github.com/fortify/fcli/releases/latest/download/fcli-windows.zip -o fcli-windows.zip
+unzip -qq -o fcli-windows.zip -d .\
+
+ECHO "### Install tools ###"
+fcli tool definitions update
+
+fcli tool fcli install --version latest -y
+
+REM # fcli tool sc-client install --version latest -y
+fcli tool sc-client install --version %SC_CLIENT_VERSION% -y
+REM # fcli tool debricked-cli install --version latest -y
+fcli tool debricked-cli install --version %DEBRICKED_CLI_VERSION% -y
+fcli tool fod-uploader install --version latest -y
+
+ECHO "### Configure SC-Client Tool ###"
+
+IF DEFINED SC_CLIENT_VERSION (
+    SET SC_CLIENT_CONFIG=%USERPROFILE%\fortify\tools\sc-client\%SC_CLIENT_VERSION%\Core\config\client.properties
+    ECHO "ScanCentral Client path=%USERPROFILE%\fortify\tools\sc-client\%SC_CLIENT_VERSION%\bin"
+    IF DEFINED SCANCENTRAL_AUTH_TOKEN (ECHO client_auth_token=%SCANCENTRAL_AUTH_TOKEN%>>%SC_CLIENT_CONFIG%)
+    IF DEFINED DEBRICKED_CLI_VERSION (
+        ECHO "Debricked CLI path=%USERPROFILE%\fortify\tools\debricked-cli\%DEBRICKED_CLI_VERSION%\bin"
+        ECHO debricked_cli_dir=%USERPROFILE%\fortify\tools\debricked-cli\%DEBRICKED_CLI_VERSION%\bin>>%SC_CLIENT_CONFIG%
+    )
+)
---
+++ fcli-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+clear
+echo "-========== INSTALL FCLI AND TOOLS SETTINGS ==========-"
+# export environment variables 
+export PATH=${HOME}/fortify/tools/bin:${PATH}
+export SC_CLIENT_VERSION=24.4.1
+export DEBRICKED_CLI_VERSION=2.6.7
+
+echo "======================================================================"
+echo " PATH=${PATH}"
+echo "SC_CLIENT_VERSION=${SC_CLIENT_VERSION}"
+echo "DEBRICKED_CLI_VERSION=${DEBRICKED_CLI_VERSION}"
+echo "======================================================================"
+
+# Download and unpack fcli 
+#curl -sL https://github.com/fortify/fcli/releases/latest/download/fcli-mac.tgz -o fcli-mac.tgz
+#tar -pxzf fcli-mac.tgz
+curl -sL https://github.com/fortify/fcli/releases/latest/download/fcli-linux.tgz -o fcli-linux.tgz
+tar -pxzf fcli-linux.tgz
+# Add completion 
+source ./fcli_completion
+
+echo "### Install tools ###"
+./fcli tool definitions update
+
+./fcli tool fcli install --version latest -y
+
+# ./fcli tool sc-client install --version latest -y
+./fcli tool sc-client install --version ${SC_CLIENT_VERSION} -y
+# ./fcli tool debricked-cli install --version latest -y
+./fcli tool debricked-cli install --version ${DEBRICKED_CLI_VERSION} -y
+./fcli tool fod-uploader install --version latest -y
+
+echo "### Configure SC-Client Tool ###"
+
+if [-v SC_CLIENT_VERSION]; then
+    export SC_CLIENT_CONFIG=${HOME}/fortify/tools/sc-client/${SC_CLIENT_VERSION}/Core/config/client.properties
+    echo "ScanCentral Client path=${HOME}/fortify/tools/sc-client/${SC_CLIENT_VERSION}/bin"
+    if [-v SCANCENTRAL_AUTH_TOKEN]; then 
+        echo client_auth_token=${SCANCENTRAL_AUTH_TOKEN}>>${SC_CLIENT_CONFIG}
+    fi
+
+    if [-v DEBRICKED_CLI_VERSION]; then
+        echo "Debricked CLI path=${HOME}/fortify/tools/debricked-cli/${DEBRICKED_CLI_VERSION}/bin" 
+        echo debricked_cli_dir=${HOME}/fortify/tools/debricked-cli/${DEBRICKED_CLI_VERSION}/bin>>${SC_CLIENT_CONFIG}
+    fi
+fi
